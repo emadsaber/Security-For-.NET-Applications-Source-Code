9efc40c1b5d180b260fd947f953bc94e62c2d0c9
Emad Saber Hamed <omda.sharkawy@live.com>
2025-02-21 23:03:27 +0200

    Lab02 - JWT Atuhentication

---
+++ 02 - JWT Authentication/JwtAuthentication/JwtAuthentication.APIs/Controllers/AuthorizationController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JwtAuthentication.APIs.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthorizationController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public record UserInfo(string username, string email, string role);
+
+        public AuthorizationController(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(string username, string password)
+        {
+            if(!CheckCredentials(username, password))
+            {
+                return Unauthorized();
+            }
+            var token = GenerateJwtToken(GetUser(username));
+            return Ok(new { token });
+        }
+
+
+        private string GenerateJwtToken(UserInfo user)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]!));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["JWT:Issuer"],
+                audience: _configuration["JWT:Audience"],
+                claims: new List<Claim>()
+                {
+                    new Claim(ClaimTypes.Name, user.username),
+                    new Claim(ClaimTypes.Email, user.email),
+                    new Claim(ClaimTypes.Role, user.role)
+                },
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private bool CheckCredentials(string username, string password)
+        {
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                return false;
+            }
+
+            // Check the username and password against the database
+            return true;
+        }
+
+        private UserInfo GetUser(string username)
+        {
+            // Get the user from the database
+            return new(username, $"{username}@expertdevs.com", "Admin");
+        }
+
+    }
+}
